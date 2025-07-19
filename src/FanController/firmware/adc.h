6a1ff417b60c483dd9f4ed41d398563c391c58fd
Josip Medved <jmedved@jmedved.com>
2025-07-18 19:47:44 -0700

    Moved fan controller to AuxFanController repo

--- src/FanController/firmware/adc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <xc.h>
-#include <stdint.h>
-
-void adc_init(void);
-uint8_t adc_get_temperature(void);
--- src/FanController/firmware/pwm.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void pwm_init(void);
-void pwm_set_all(uint8_t pwm);
-void pwm_set_individual(uint8_t pwm1, uint8_t pwm2, uint8_t pwm3, uint8_t pwm4);
